ff2c71666a0aae16159d70a23126b848dd2249c2
Mikolaj Cepil <Mikolaj.Cepil@ge.com>
2017-09-12 13:39:18 +0200

    Automating coverage report generation after building tests

--- MyWpfApp.Tests/GenerateTestCoverage.bat
+++ MyWpfApp.Tests/GenerateTestCoverage.bat
@@ -1,5 +1,7 @@
-cd MyWpfApp.Tests\bin\Debug\
+cd bin\Debug\
 
 ..\..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"..\..\..\packages\xunit.runner.console.2.2.0\tools\xunit.console.exe" -targetargs:"\"MyWpfApp.Tests.dll\" -xml \"..\..\..\TestResults\MyWpfApp.Tests.xunit.xml\"" -output:"..\..\..\TestResults\coverage.xml" -skipautoprops -returntargetcode -filter:"+[MyWpfApp*]* -[MyWpfApp.Tests]*
 
 ..\..\..\packages\ReportGenerator.2.5.11\tools\ReportGenerator.exe -reports:"..\..\..\TestResults\coverage.xml" -targetdir:"..\..\..\TestResults\TestCoverage"
+
+cd ..\..\
--- MyWpfApp/CSharp6/UsersExtensions.cs
+++ MyWpfApp/CSharp6/UsersExtensions.cs
@@ -4,7 +4,7 @@
     {
         //This extension will be used in Users initializers, since the class
         //itself does not define a Add() method - this wasn't possible before C# 6
-        //<see test AssersUsersCanAddUser>
+        //<see test AssertUsersCanAddUser>
         public static User Add(this Users users, User newUser)
         {
             return users.AddUser(newUser);
